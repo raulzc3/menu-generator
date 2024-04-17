365a21317ec57c4b5b01073082649333da3bd264
raulzc3 <raulzc3pc@gmail.com>
2024-04-17 23:02:21 +0200

    v1.07.01

---
+++ CHANGELOG.md
@@ -0,0 +1,13 @@
+# 1.07.01
+
+### Major changes:
+
+- Add i18n:
+  - English
+  - Spanish
+  - Galician
+
+### Minor changes:
+
+- Start changelog
+- Update libraries
--- src/App.js
+++ src/App.js
@@ -95,7 +95,7 @@ function App() {
               }}
             >
               <Stack gap={2}>
-                <Text size="xs">v1.6.03</Text>
+                <Text size="xs">v1.07.01</Text>
                 <Text size="xs">
                   {" "}
                   {t("nav_icon_text")}
